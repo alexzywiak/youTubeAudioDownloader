5548018c8e967c167e33174f9e3dcf32ef82e865
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-05 11:50:00 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # youTubeAudioDownloader
 Downloads mp3 audio from a youtube video to the server. <br>
+Make sure to install youtube-dl and ffmpeg first. <br>
+If you have homebrew: brew install youtube-dl, brew install ffmpeg <br>
+
 1) npm install <br>
 2) node bin/www <br>
-3) localhost:3000/videos/(:youtube video id) <br>
-4) localhost:3000/videos/IONyLZn0pLI <br>
+3) Navigate to: localhost:3000/videos/(:youtube video id) <br>
+4) To get started: localhost:3000/videos/IONyLZn0pLI <br>
--- routes/videos.js
+++ routes/videos.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/:video_id', function(req, res, next) {
+	// Install youtube_dl locally: brew install youtube-dl
   youtube_dl = spawn('youtube-dl', ['--extract-audio', '--audio-format', 'mp3', "http://www.youtube.com/watch?v=" + req.params.video_id]);
 
   youtube_dl.stdout.on('data', function(data){
@@ -16,9 +17,11 @@ router.get('/:video_id', function(req, res, next) {
   	process.stderr.write(data);
   });
 
+  // Install ffmpeg locally
+  // brew install ffmpeg
   youtube_dl.on('exit', function(){
   	fs.readFile('./' + req.params.video_id + '.mp3', function(err, data){
-  		res.render('/index', {title: 'Scrape Some Audio'});
+  		res.render('index', {title: 'Scrape Some Audio'});
   	});
   });
 });
